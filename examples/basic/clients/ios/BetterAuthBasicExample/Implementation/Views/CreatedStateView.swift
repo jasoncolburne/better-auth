cf6ebab3d917c40dd5221973a3dfd1a70078f484
Jason Colburne <j.colburne@gmail.com>
2025-10-24 05:11:55 -0300

    unlink device

--- examples/basic/clients/ios/BetterAuthBasicExample/Implementation/Views/CreatedStateView.swift
+++ examples/basic/clients/ios/BetterAuthBasicExample/Implementation/Views/CreatedStateView.swift
@@ -3,8 +3,12 @@ import SwiftUI
 struct CreatedStateView: View {
     @Binding var isLoading: Bool
     @Binding var showLinkDevicePrompt: Bool
+    @Binding var deviceValue: String
+
     let onCreateSession: () async -> Void
+    let onUnlinkDevice: () async -> Void
     let onDeleteAccount: () async -> Void
+    let onEraseCredentials: () async -> Void
 
     var body: some View {
         Group {
@@ -54,6 +58,33 @@ struct CreatedStateView: View {
             .disabled(isLoading)
             .padding(.horizontal)
 
+            TextField("Device to unlink", text: $deviceValue)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal)
+
+            Button(action: {
+                Task {
+                    await onUnlinkDevice()
+                }
+            }) {
+                HStack {
+                    if isLoading {
+                        ProgressView()
+                            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+                            .frame(width: 20, height: 20)
+                    }
+                    Text("Unlink device")
+                        .fontWeight(.semibold)
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(isLoading ? Color.gray : Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+            }
+            .disabled(isLoading)
+            .padding(.horizontal)
+
             Button(action: {
                 Task {
                     await onDeleteAccount()
@@ -66,6 +97,29 @@ struct CreatedStateView: View {
                             .frame(width: 20, height: 20)
                     }
                     Text("Delete account")
+                        .fontWeight(.semibold)
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(isLoading ? Color.gray : Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+            }
+            .disabled(isLoading)
+            .padding(.horizontal)
+
+            Button(action: {
+                Task {
+                    await onEraseCredentials()
+                }
+            }) {
+                HStack {
+                    if isLoading {
+                        ProgressView()
+                            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+                            .frame(width: 20, height: 20)
+                    }
+                    Text("Erase credentials")
                         .fontWeight(.semibold)
                 }
                 .frame(maxWidth: .infinity)
